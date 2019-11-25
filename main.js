6d4728eab4c59a9834743c7cb401b8221b9a240e
lucapu88 <caputoluca88@gmail.com>
2019-11-25 17:00:07 +0100

    commenti

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // aggiungere le istruzioni jQuery necessarie e sufficienti per attivare l'hamburger menu.
 // La struttura html e lo stile presente contengono già tutto l'occorrente per l'hamburger menu, ciò che dovete fare è capire com'è predisposto il tutto e incastrare le istruzioni js corrette per mostrare e nascondere l'hamburger menu su mobile.
 
-
+//cliccando sull'icona dell'hamburger faccio apparire il menu
   $('.header-right > a').click(function() {
     $('.hamburger-menu').removeClass('.active');
   });
 
+//cliccando sull'icona della 'x' faccio scomparire il menu
   $('.close').click(function() {
     $('.hamburger-menu').addClass('.active');
   });
